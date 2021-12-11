ef76a4db6177c98f4944ea13202521fddb7c056b
Ronsip <95915577+Ronsip@users.noreply.github.com>
2021-12-11 09:42:25 +0100

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -47,7 +47,7 @@ class CreditsState extends MusicBeatState
 
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
 			['kvrtecek team'],
-			['kada and kubiiicek',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
+			['kada and kubiiicek',		    'majigsaw',		    'creators of vs kvrtecek',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'0xFFC30085'],
